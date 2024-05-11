85ed141d83ebd862c45598834cdbeffeb836b0c0
Jacobs <yakovsobolev1@gmail.com>
2024-05-11 21:34:54 +0300

    add state for input url

--- src/components/InputArea/InputArea.jsx
+++ src/components/InputArea/InputArea.jsx
@@ -1,16 +1,29 @@
-import "./InputArea.css";
-import React from "react";
+import React, { useState } from "react";
+
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+import "./InputArea.css";
+
 function InputArea() {
+  const [inputUrl, setInputUrl] = useState("");
+
+  function handelInputChange(event) {
+    setInputUrl(event.target.value);
+  }
+
   return (
     <Container className="container" maxWidth="lg">
       <Grid container spacing={2} alignItems="flex-end">
         <Grid item xs={9}>
-          <TextField fullWidth label="YouTube Playlist URL" />
+          <TextField
+            fullWidth
+            label="YouTube Playlist URL"
+            value={inputUrl}
+            onChange={handelInputChange}
+          />
         </Grid>
         <Grid item xs>
           <Button variant="contained" fullWidth={true}>
