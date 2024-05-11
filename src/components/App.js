fea9a6523ea6a1a5da67f736eca06d970f221d97
Jacobs <yakovsobolev1@gmail.com>
2024-05-11 20:55:34 +0300

    Input Area component init

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import InputArea from "./InputArea/InputArea";
 
 function App() {
   return (
     <div>
       <Header />
+      <InputArea />
       <Footer />
     </div>
   );
---
+++ src/components/InputArea/InputArea.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputArea() {
+  return <div>Input area</div>;
+}
+
+export default InputArea;
