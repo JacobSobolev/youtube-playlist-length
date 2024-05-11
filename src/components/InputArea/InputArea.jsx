076d007dae01eaf02f9f765a703dc85512710e32
Jacobs <yakovsobolev1@gmail.com>
2024-05-11 21:30:45 +0300

    add input text and button in a grid

--- src/components/InputArea/InputArea.jsx
+++ src/components/InputArea/InputArea.jsx
@@ -1,7 +1,25 @@
+import "./InputArea.css";
 import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function InputArea() {
-  return <div>Input area</div>;
+  return (
+    <Container className="container" maxWidth="lg">
+      <Grid container spacing={2} alignItems="flex-end">
+        <Grid item xs={9}>
+          <TextField fullWidth label="YouTube Playlist URL" />
+        </Grid>
+        <Grid item xs>
+          <Button variant="contained" fullWidth={true}>
+            Calculate
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default InputArea;
