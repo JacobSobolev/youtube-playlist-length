ebf6174f4559bb436ef35f2db0de0c10ea4052fc
Jacobs <yakovsobolev1@gmail.com>
2024-05-11 20:47:12 +0300

    update component text and css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <footer>
-    footer
-  </footer>;
+  return (
+    <footer>
+      <p>Made by Jacob Sobolev</p>
+    </footer>
+  );
 }
 
 export default Footer;
