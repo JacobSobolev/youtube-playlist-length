7d883c14349318a0d8441e999b22104e4ef299d5
Jacobs <yakovsobolev1@gmail.com>
2024-05-13 10:57:24 +0300

    getting data about the playlist

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,37 @@ import InputArea from "./InputArea/InputArea";
 import "./App.css";
 
 function App() {
-  function calcList(url) {
-    console.log(url);
+  const YoutubeListApi =
+    "https://www.googleapis.com/youtube/v3/playlists?part=snippet" +
+    "&id=PL59FEE129ADFF2B12&key=" +
+    process.env.REACT_APP_API_KEY;
+
+  const YoutubeListItemsApi =
+    "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50" +
+    "&playlistId=PL59FEE129ADFF2B12&key=" +
+    process.env.REACT_APP_API_KEY;
+
+  async function getYoutubeListItemsData(url) {
+    const res = await fetch(YoutubeListItemsApi);
+    const data = await res.json();
+    return data;
+  }
+
+  async function getYoutubeListData(url) {
+    const res = await fetch(YoutubeListApi);
+    const data = await res.json();
+    return data;
+  }
+
+  async function calcList(url) {
+    try {
+      const data = await getYoutubeListItemsData(url);
+      console.log(data);
+      const data2 = await getYoutubeListData(url);
+      console.log(data2);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
