67694a8dcc91bcc053060927449b275d3e7bd7bd
Jacobs <yakovsobolev1@gmail.com>
2024-05-24 18:46:44 +0300

    change link to chip

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import "./Footer.css";
 
 import Container from "@mui/material/Container";
-import Link from "@mui/material/Link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Chip from "@mui/material/Chip";
 
 function Footer() {
   return (
     <Container className="container" maxWidth="lg">
       <footer>
-        <p>Made by Jacob Sobolev</p>
-        <p>
-          <Link href="https://github.com/JacobSobolev/youtube-playlist-length" underline="hover">
-            Source Code Here
-          </Link>
-        </p>
+        
+        <a href="https://github.com/JacobSobolev/youtube-playlist-length">
+          <Chip icon={<GitHubIcon />} label="Made by Jacob Sobolev" color="primary" variant="outlined"/>
+        </a>
       </footer>
     </Container>
   );
