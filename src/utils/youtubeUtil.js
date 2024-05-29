c065e85d69e8f9e9d690ccb3538da4f05bde9c0e
Jacobs <yakovsobolev1@gmail.com>
2024-05-29 15:04:42 +0300

    convert string concatenation to template literals

--- src/utils/youtubeUtil.js
+++ src/utils/youtubeUtil.js
@@ -1,22 +1,17 @@
 import Moment from "moment";
 
-const YoutubeListApi =
-  "https://www.googleapis.com/youtube/v3/playlists?part=snippet" +
-  "&key=" +
-  process.env.REACT_APP_API_KEY;
+const YoutubeListApi = `https://www.googleapis.com/youtube/v3/playlists?part=snippet&key=${process.env.REACT_APP_API_KEY}`;
 
 async function getYoutubeListData(listId) {
-  const res = await fetch(YoutubeListApi + "&id=" + listId);
+  const res = await fetch(`${YoutubeListApi}&id=${listId}`);
+
   const {
     items: [{ snippet }],
   } = await res.json();
   return snippet;
 }
 
-const YoutubeListItemsApi =
-  "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50" +
-  "&key=" +
-  process.env.REACT_APP_API_KEY;
+const YoutubeListItemsApi = `https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&key=${process.env.REACT_APP_API_KEY}`;
 
 async function getYoutubeListItemsData(listId) {
   let videoListTime = [];
@@ -25,7 +20,7 @@ async function getYoutubeListItemsData(listId) {
 
   do {
     const res = await fetch(
-      YoutubeListItemsApi + "&playlistId=" + listId + "&pageToken=" + pageToken
+      `${YoutubeListItemsApi}&playlistId=${listId}&pageToken=${pageToken}`
     );
     const { items, nextPageToken } = await res.json();
     pageToken = nextPageToken;
@@ -40,14 +35,11 @@ async function getYoutubeListItemsData(listId) {
   return videoListTime;
 }
 
-const YoutubeVideoApi =
-  "https://www.googleapis.com/youtube/v3/videos?part=contentDetails" +
-  "&key=" +
-  process.env.REACT_APP_API_KEY;
+const YoutubeVideoApi = `https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=${process.env.REACT_APP_API_KEY}`;
 
 async function getVideosDuration(videoIds) {
   let videosDuration = [];
-  const res = await fetch(YoutubeVideoApi + "&id=" + videoIds);
+  const res = await fetch(`${YoutubeVideoApi}&id=${videoIds}`);
   const { items } = await res.json();
 
   videosDuration = [
